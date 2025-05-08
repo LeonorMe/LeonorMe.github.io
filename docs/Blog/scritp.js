502c5e8e681df43a6e729e463d02e5a117df8d30
Leonor <53794672+LeonorMe@users.noreply.github.com>
2025-05-08 22:55:38 +0100

    Blog updated index

--- docs/Blog/scritp.js
+++ docs/Blog/scritp.js
@@ -1,4 +1,26 @@
-$(function(){
-    $("header").load("header.html"); 
-    $("footer").load("footer.html"); 
+
+/*document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    const footer = document.querySelector("footer");
+    header.innerHTML = loadPager("/subpages/header.html");
+    footer.innerHTML = loadPager("/subpages/footer.html");
+
 });
+
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+header.innerHTML = loadPager("subpages/header.html");
+footer.innerHTML = loadPager("subpages/footer.html");
+
+const lastPost = document.querySelector(".last-post");
+const posts = document.querySelector(".posts");
+lastPost.innerHTML = loadPager("subpages/lastPost.html");
+posts.innerHTML = loadPager("subpages/posts.html");
+*/
+
+    //$("header").load("subpages/header.html"); 
+    //$("footer").load("footer.html"); 
+    //$(".last-post").load("/subpages/lastPost.html"); 
+    //$(".posts").load("/subpages/posts.html");
+
+
