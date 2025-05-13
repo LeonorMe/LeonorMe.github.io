9c711f733777e28906fae3178af14e9e9bee8110
Leonor <53794672+LeonorMe@users.noreply.github.com>
2025-05-13 16:59:09 +0100

    Added Blog page

--- docs/Blog/getPosts.js
+++ docs/Blog/getPosts.js
@@ -40,7 +40,7 @@ function getLastPost() {
     })
 };
 
-// All posts
+// All posts - last post
 function getPosts() {
     fetch("posts.json")
         .then((response) => response.json())
@@ -82,6 +82,53 @@ function getPosts() {
             }
         })
 };
+
+// All posts
+function getAllPosts() {
+    fetch("posts.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const pSection = document.querySelector(".posts");
+            
+            // TODO: limit to 9 posts until scroll down
+            
+            // go trow data from data.length - 1 to 0
+            for (let i = data.length - 1; i >= 0; i--) {
+                const pData = data[i];
+                const pArticle = document.createElement("article");
+                pArticle.classList.add("post");
+        
+                //TODO: add topic to posts.json  link.href = "detail.html?id=" + p.id;
+                // TODO: suport more than 1 tag
+        
+                pArticle.innerHTML = `
+                    <div class="row">
+                        <div>      
+                            <img src="${pData.images[0]}" alt="post-cover-image">
+                        </div>  
+                        <div class="col-gold-small">
+                            <div class="topics">
+                                <a href="#">${pData.tags[0]}</a> 
+                            </div>
+                            <h2 class="title">${pData.title}</h2>   
+                            <span class="date">${pData.date}</span>
+                            
+                        </div>
+                        
+                        <div class="abstract">${pData.abstract}</div>
+                    </div>
+                `;
+
+                // On click article go to detail.html?id=lpData.id
+                pArticle.addEventListener("click", () => {
+                    window.location.href = "detail.html?id=" + pData.id;
+                });
+        
+                pSection.appendChild(pArticle);
+            }
+        })
+};
+
 
 // Post Detail
 function getPostDetail() {
